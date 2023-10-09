036a0111b7cf2d4fa03896f5f101d11a8221a27c
Dmitriy Dmitriy <dbilica19@gmail.com>
2023-10-09 16:37:15 +0300

    Добавил задание с массивами

---
+++ massiv/Price.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Количество поступивщих товаров: ");
+int price = int.Parse(Console.ReadLine());
+
+int[] prices = new int[price];
+
+for (int i = 0; i < prices.Length; i++)
+{
+    Console.Write("Цена товара от поставщика: ");
+    prices[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < prices.Length; i++)
+{
+    prices[i] += prices[i] * 18 / 100;
+
+    Console.WriteLine($"Цена для продажи: " + prices[i]);
+}
