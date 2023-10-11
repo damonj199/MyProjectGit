50da4afb9488a62413d324e3d5f718eb0720ee05
Dmitriy Dmitriy <dbilica19@gmail.com>
2023-10-11 17:57:10 +0300

    сделал все задания в одном месте, Урок введение в массивы

--- massiv/Price.cs
+++ massiv/Price.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Количество поступивщих товаров: ");
+﻿//Задание 1
+
+Console.Write("Количество поступивщих товаров: ");
 int price = int.Parse(Console.ReadLine());
 
 int[] prices = new int[price];
@@ -14,3 +16,51 @@ for (int i = 0; i < prices.Length; i++)
 
     Console.WriteLine($"Цена для продажи: " + prices[i]);
 }
+//Задание 2
+
+Console.Write("Введите количество сотрудников: ");
+int rating = int.Parse(Console.ReadLine());
+
+int[] ratings = new int[rating];
+
+for (int i = 0; i < ratings.Length; i++)
+{
+    Console.Write("Укажите рейтинг сотрудника: ");
+    ratings[i] = int.Parse(Console.ReadLine());
+}
+
+int maxRating = ratings[0];
+
+for (int i = 0; i < ratings.Length; i++)
+{
+    if (maxRating < ratings[i])
+    {
+        maxRating = ratings[i];
+    }
+}
+Console.WriteLine($"Наибольший рейтинг сотрудника: " + maxRating);
+
+//Задание 3
+
+Console.Write("Количество работников: ");
+int salePlan = int.Parse(Console.ReadLine());
+
+Console.Write("Укажите план продаж: ");
+int plan = int.Parse(Console.ReadLine());
+
+int[] salePlans = new int[salePlan];
+int staff = 0;
+
+for (int i = 0; i < salePlans.Length; i++)
+{
+    Console.Write("Сумма продаж каждого сотрудника: ");
+    salePlans[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < salePlans.Length; i++)
+{
+    if (salePlans[i] < plan)
+    {
+        staff++;
+    }
+}
+Console.WriteLine($"Cотрудников не выполнивших план: " + staff);
